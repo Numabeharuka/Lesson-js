a286731ca257b93f0f7fd5ebc81b7cc6fe24a544
沼辺晴華 <numabeharuka@MacBook-Air.local>
2022-02-18 12:01:57 +0900

    refactor: script.js

--- lesson_02/script.js
+++ lesson_02/script.js
@@ -1,4 +1,20 @@
 "use strict";
 
-const ul = document.getElementById("target");
-ul.innerHTML ='<li><a href="1.html"><img src="bookmark.png" alt="ブックマーク">これです</a></li>';
+//<li>
+//<a href="1.html"><img src="bookmark.png" alt="ブックマーク" />これです</a>
+//</li>
+
+const li = document.createElement("li"); //<li></li>
+
+const a = document.createElement("a"); //<a href="1.html"></a>
+a.text = "これです";
+a.href = "1.html";
+
+const img = document.createElement("img"); //<img>
+img.src = "bookmark.png";
+img.alt = "ブックマーク";
+
+const ul = document.getElementById("target"); //ulを取得
+ul.appendChild(li); //ulにli追加
+li.appendChild(a); //liにa追加
+a.insertAdjacentElement("beforebegin", img); //aにimg追加
