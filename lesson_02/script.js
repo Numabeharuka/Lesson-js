9ef8c792ad3eeeab190cf0e447a81349ca4743b1
沼辺晴華 <numabeharuka@MacBook-Air.local>
2022-02-21 22:52:24 +0900

    style: Crrection after review
    Add js to ID. Change the code style of js

--- lesson_02/script.js
+++ lesson_02/script.js
@@ -1,14 +1,14 @@
 "use strict";
 
+const ul = document.getElementById("js-target");
 const li = document.createElement("li");
-
 const a = document.createElement("a");
 a.text = "これです";
 a.href = "1.html";
 
 const img = document.createElement("img");
+img.src = "bookmark.png";
 img.alt = "ブックマーク";
 
-const ul = document.getElementById("target");
 ul.appendChild(li);
 li.appendChild(a).insertAdjacentElement("afterbegin", img);
