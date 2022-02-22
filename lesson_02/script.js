75e0a702083f673f18fc854739bf2fd1e76f4b20
沼辺晴華 <numabeharuka@MacBook-Air.local>
2022-02-22 16:08:30 +0900

    style: organize by element

--- lesson_02/script.js
+++ lesson_02/script.js
@@ -3,10 +3,10 @@
 const ul = document.getElementById("js-target");
 const li = document.createElement("li");
 const a = document.createElement("a");
+const img = document.createElement("img");
+
 a.text = "これです";
 a.href = "1.html";
-
-const img = document.createElement("img");
 img.src = "bookmark.png";
 img.alt = "ブックマーク";
 
