018dc587464112bd32d50d805e5bd2d9dfc7915a
沼辺晴華 <numabeharuka@MacBook-Air.local>
2022-02-24 15:24:13 +0900

    style: iteration

--- lesson_03/script.js
+++ lesson_03/script.js
@@ -1,23 +1,12 @@
 "use strict";
-
 const ul = document.getElementById("js-target");
-const liFirst = document.createElement("li");
-const aFirst = document.createElement("a");
-const imgFirst = document.createElement("img");
-const liLast = document.createElement("li");
-const aLast = document.createElement("a");
-const imgLast = document.createElement("img");
+for (let i = 1; i <= 2; i += 1) {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  const img = document.createElement("img");
+  a.text = "a" + i;
+  a.href = "a" + i + ".html";
+  img.src = "/img/bookmark.png";
 
-aFirst.text = "a1";
-aFirst.href = "a1.html";
-imgFirst.src = "/img/bookmark.png";
-aLast.text = "a2";
-aLast.href = "a2.html";
-imgLast.src = "/img/bookmark.png";
-
-ul.appendChild(liFirst)
-  .appendChild(aFirst)
-  .insertAdjacentElement("afterbegin", imgFirst);
-ul.appendChild(liLast)
-  .appendChild(aLast)
-  .insertAdjacentElement("afterbegin", imgLast);
+  ul.appendChild(li).appendChild(a).insertAdjacentElement("afterbegin", img);
+}
