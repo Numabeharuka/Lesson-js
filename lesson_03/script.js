10a1196e859cbb4eb90980390dbfd04357454a47
沼辺晴華 <numabeharuka@MacBook-Air.local>
2022-02-26 23:51:07 +0900

    style: data addition

--- lesson_03/script.js
+++ lesson_03/script.js
@@ -1,11 +1,15 @@
 "use strict";
 const ul = document.getElementById("js-target");
-for (let i = 1; i <= 2; i += 1) {
+const data = ["a1.html", "a2.html"];
+const listCount = function () {
+  return 2;
+};
+for (let i = 1; i <= listCount(); i += 1) {
   const li = document.createElement("li");
   const a = document.createElement("a");
   const img = document.createElement("img");
-  a.text = "a" + i;
-  a.href = "a" + i + ".html";
+  a.text = `a${i}`;
+  a.href = `${data[i - 1]}`;
   img.src = "/img/bookmark.png";
 
   ul.appendChild(li).appendChild(a).insertAdjacentElement("afterbegin", img);
