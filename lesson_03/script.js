48f3102883bf9942c80ee01644f202356c9bb79c
沼辺晴華 <numabeharuka@MacBook-Air.local>
2022-02-27 11:47:11 +0900

    style: function erasure

--- lesson_03/script.js
+++ lesson_03/script.js
@@ -1,11 +1,8 @@
 "use strict";
 const ul = document.getElementById("js-target");
 const data = ["a1.html", "a2.html"];
-const listCount = function () {
-  return 2;
-};
 const fragment = document.createDocumentFragment();
-for (let i = 1; i <= listCount(); i += 1) {
+for (let i = 1; i <= data.length; i += 1) {
   const li = document.createElement("li");
   const a = document.createElement("a");
   const img = document.createElement("img");
@@ -13,6 +10,6 @@ for (let i = 1; i <= listCount(); i += 1) {
   a.href = `${data[i - 1]}`;
   img.src = "/img/bookmark.png";
 
-  fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);
+  fragment.appendChild(li).appendChild(a).insertAdjacentElement("afterbegin", img);
 }
 ul.appendChild(fragment);
