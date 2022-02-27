f664f5dc89140e568951ae1447332aecf5465d77
沼辺晴華 <numabeharuka@MacBook-Air.local>
2022-02-28 01:25:33 +0900

    style: Template literal

--- lesson_03/script.js
+++ lesson_03/script.js
@@ -2,12 +2,12 @@
 const ul = document.getElementById("js-target");
 const data = ["a1.html", "a2.html"];
 const fragment = document.createDocumentFragment();
-for (let i = 1; i <= data.length; i += 1) {
+for (let i = 1; i <= data.length; i ++) {
   const li = document.createElement("li");
   const a = document.createElement("a");
   const img = document.createElement("img");
   a.text = `a${i}`;
-  a.href = `${data[i - 1]}`;
+  a.href = data[i-1];
   img.src = "/img/bookmark.png";
 
   fragment.appendChild(li).appendChild(a).insertAdjacentElement("afterbegin", img);
