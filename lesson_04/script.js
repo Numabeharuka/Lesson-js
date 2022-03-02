dc2bd1f8932b3befc274eee293765b1ced34f90d
沼辺晴華 <numabeharuka@MacBook-Air.local>
2022-03-02 23:45:00 +0900

    feat: Js image added

---
+++ lesson_04/script.js
@@ -0,0 +1,20 @@
+"use strict";
+const ul = document.getElementById("js-target");
+const fragment = document.createDocumentFragment();
+const data = [
+  { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
+  { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" }
+];
+for (let i = 0; i <= data.length - 1; i++) {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  const img = document.createElement("img");
+  a.href = data[i].to;
+  a.text = data[i].text;
+  img.alt = data[i].alt;
+  fragment
+    .appendChild(li)
+    .appendChild(a)
+    .insertAdjacentElement("afterbegin", img);
+}
+ul.appendChild(fragment);
