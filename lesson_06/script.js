911a1b9610cdc6b4fa24627f7da5ba9da8d9e68c
沼辺晴華 <numabeharuka@MacBook-Air.local>
2022-03-17 11:56:35 +0900

    style: promise

--- lesson_06/script.js
+++ lesson_06/script.js
@@ -1,23 +1,23 @@
 "use strict";
 const ul = document.getElementById("js-list");
-const fragment = document.createDocumentFragment();
 const data = [
   { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
   { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" }
 ];
 
-const promise = new Promise((resolve) => {
+const data = new Promise((resolve) => {
   setTimeout(() => {
     resolve(data);
   }, 3000);
 });
-promise.then((value) => {
+data.then((value) => {
+  const fragment = document.createDocumentFragment();
   for (let i = 0; i < value.length; i++) {
     const li = document.createElement("li");
     const a = document.createElement("a");
     const img = document.createElement("img");
     a.href = value[i].to;
-    a.text = value[i].text;
+    a.textContent = value[i].text;
     img.src = value[i].img;
     img.alt = value[i].alt;
     fragment
